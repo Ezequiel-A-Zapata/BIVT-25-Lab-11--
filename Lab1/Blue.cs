a366cac280de1be9b68f69dd7dbe3cd6c10b7804
Ezequiel Alejandro Zapata <157446504+Ezequiel-A-Zapata@users.noreply.github.com>
2025-09-13 00:20:09 +0300

    BlueList

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,7 +7,19 @@
             bool answer = false;
 
             // code here
-sdfasdsdf
+
+                if (a > 0 && b > 0)
+                {
+                    answer = true;
+                }
+                else if (a < 0 && b < 0)
+                {
+                    answer = true;
+                }
+                else
+                {
+                    answer = false;
+                }
             // end
 
             return answer;
@@ -17,7 +29,12 @@ sdfasdsdf
             bool answer = false;
 
             // code here
-
+            double n = Math.Truncate(d);
+            double f = Math.Abs(d - n);
+            if (f >= 0.0001)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,7 +44,10 @@ sdfasdsdf
             bool answer = false;
 
             // code here
-
+            if (b != 0 && a % b == 0)
+            {
+                answer = true;        
+            }
             // end
 
             return answer;
@@ -37,7 +57,12 @@ sdfasdsdf
             double answer = 0;
 
             // code here
+            double abs_d = Math.Abs(d);
+            double abs_f = Math.Abs(f);
+            double abs_g = Math.Abs(g);
 
+            double n_max = Math.Max(abs_d, Math.Max(abs_f, abs_g));
+            answer = n_max;
             // end
 
             return answer;
@@ -47,7 +72,18 @@ sdfasdsdf
             double answer = 0;
 
             // code here
-
+            if (x < +-1)
+            {
+                answer = 0;
+            }
+            else if (x > -1 && x <= 0)
+            {
+                answer = x + 1;
+            }
+            else
+            {
+                answer = 1;
+            }
             // end
 
             return answer;
@@ -57,7 +93,12 @@ sdfasdsdf
             bool answer = false;
 
             // code here
-
+            double r = Math.Sqrt(circleS / Math.PI);
+            double a = Math.Sqrt(squareS);
+            if (2 * r <= a)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +109,20 @@ sdfasdsdf
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = 1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
             // end
 
             return answer;
@@ -78,12 +132,27 @@ sdfasdsdf
             bool answer = false;
 
             // code here
-            
+            int A = a / 2;
+            int B = b / 2;
+            int C = c / 2;
+            int S = A + B + C;
+
+            if (S % 3 == 0)
+            {
+                int k = S / 3;
+                if (k >= 1 && k <= a && k <= b && k <= c)
+                    answer = true;
+            }
+
+            if (!answer && (S + 1) % 3 == 0)
+            {
+                int k = (S + 1) / 3;
+                if (k >= 1 && k <= a && k <= b && k <= c)
+                    answer = true;
+            }
             // end
 
             return answer;
         }
     }
-
-}
-
+}  
